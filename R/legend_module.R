c9260aee288d3618f1a1df6ccc53cf2f0a3669cc
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2023-02-18 10:30:16 -0500

    legend_ui and legend_server on the same documentation. tryCatch added to make sure the app doesn't break when the legend module fails. Returns nothing instead, with a warning.

--- R/legend_module.R
+++ R/legend_module.R
@@ -1,21 +1,15 @@
-#' Create the UI for the legend module
+#' Create the UI and server logic for the legend module
 #'
-#' @param id <`character`> The ID of the page in which the legend will appear,
-#' e.g. `canale`.
-#'
-#' @return The legend Shiny UI object
-#' @export
-legend_UI <- function(id) {
-  shiny::div(
-    id = shiny::NS(id, "legend_div"),
-    shiny::h5(cc_t("Legend"),
-      style = "font-size: 12px;"
-    ),
-    shiny::uiOutput(shiny::NS(id, "legend_render"))
-  )
-}
-
-#' Create the server logic for the legend module
+#' The height of the legend plot is determined based on the the class of the
+#' `vars` object. If the `bivar` string is detected in in the class, then the
+#' height is set to 150 pixels. In all other cases, the height is 60 pixels.
+#' This ensures that the plot legend is sized appropriately for the content
+#' of the plot (`bivar` plot is displayed as a 3x3 blocks square). Around the
+#' \code{\link[curbcut]legend_render} function, there is a tryCatch used to
+#' catch any errors that occur when rendering the legend. If an error occurs,
+#' the error message is printed to the console and the function returns
+#' \code{NULL}. This ensures that the Shiny application continues to function
+#' even if there is an error with the legend module.
 #'
 #' @param id <`character`> The ID of the page in which the legend will appear,
 #' e.g. `canale`.
@@ -34,7 +28,7 @@ legend_UI <- function(id) {
 #' @param breaks <`reactive numeric vector`> Breaks if they need to be manually
 #' supplied to the legend module.
 #'
-#' @return The legend Shiny server function
+#' @return The legend Shiny UI and server functions
 #' @export
 legend_server <- function(id, r, vars, data, df, hide = shiny::reactive(FALSE),
                           build_str_as_DA = shiny::reactive(TRUE),
@@ -49,25 +43,25 @@ legend_server <- function(id, r, vars, data, df, hide = shiny::reactive(FALSE),
   shiny::moduleServer(id, function(input, output, session) {
     # Define plot height
     plot_height <- function() {
-      if ((length(vars()$var_left) == 1 && vars()$var_right[1] == " ") ||
-        (length(vars()$var_left) == 2 && vars()$var_right[1] == " ")) {
-        60
-      } else {
-        150
-      }
+      if (grepl("bivar", attr(vars(), "class"))) return(150)
+      return(60)
     }
 
     # Make legend
     legend <- shiny::reactive(
-      legend_render(
+      tryCatch(legend_render(
         vars = vars(),
         lang = r$lang(),
         data = data(),
         df = df(),
         build_str_as_DA = build_str_as_DA(),
         breaks = breaks()
+      ), error = function(e) {
+        # If does not work as intended, warn the error and return nothing
+        print(warning(e))
+        return(NULL)
+      })
       )
-    )
 
     # Output legend
     output$legend_render <- shiny::renderUI({
@@ -82,4 +76,16 @@ legend_server <- function(id, r, vars, data, df, hide = shiny::reactive(FALSE),
     # Toggle legend display
     shiny::observe(shinyjs::toggle("legend_div", condition = !is.null(legend())))
   })
+}
+
+#' @describeIn legend_server Create the UI for the legend module
+#' @export
+legend_UI <- function(id) {
+  shiny::div(
+    id = shiny::NS(id, "legend_div"),
+    shiny::h5(cc_t("Legend"),
+              style = "font-size: 12px;"
+    ),
+    shiny::uiOutput(shiny::NS(id, "legend_render"))
+  )
 }
