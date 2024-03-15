7d42cd634cb9e712e96b38e06717826495ddcbf2
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2024-03-15 10:45:04 -0400

    fixed page ndvi

--- R/page_ndvi.R
+++ R/page_ndvi.R
@@ -248,7 +248,7 @@ map_scale_fill_ndvi <- function(vars, time) {
     # Update the `r[[id]]$vars` reactive
     update_vars(
       id = id, r = r, var_left = var_left,
-      var_right = var_right, widget_time = widget_time
+      var_right = var_right, scale = r[[id]]$scale, widget_time = widget_time
     )
 
     # Sidebar
